bd906c6188ba671c9a13b5a1a63adce215f0e8ca
medina122 <46516881+medina122@users.noreply.github.com>
2023-03-20 05:18:36 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,2 +1,2 @@
-openai_token = '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'
+openai_token = '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'
 telegram_token = '1759121577:AAHVHZMjB8cFkxzflzcJbNz1C4vLecxzOrg'
